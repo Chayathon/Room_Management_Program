397cf38ccb3f3782482133f0092f0f04f44dd52b
Chayathon <gusgus157903@gmail.com>
2024-03-05 23:28:43 +0700

    05-03-2024

--- rent_room.java
+++ rent_room.java
@@ -3,55 +3,54 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class rent_room {
-    JFrame window = new JFrame("Rent Rooms");
-    JLabel textLabel;
-    JTextField textField;
-    JTextArea textArea;
-    JButton confirmButton;
-    JRadioButton radioButton;
-    JCheckBox checkBox;
-    ButtonGroup buttonGroup;
+    JFrame frame = new JFrame("Rent Rooms");
+    JRadioButton fanButton, airButton;
     Container container;
 
     public rent_room() {
-        container = window.getContentPane();
-        container.setLayout( new FlowLayout(FlowLayout.LEFT, 10, 10) );
+        container = frame.getContentPane();
+        container.setLayout(new FlowLayout(1, 10, 10));
 
-        textLabel = new JLabel("ชื่อ : ");
-        container.add(textLabel);
-        textField = new JTextField(20);
-        container.add(textField);
-        
-        textLabel = new JLabel("นามสกุล : ");
-        container.add(textLabel);
-        textField = new JTextField(20);
-        container.add(textField);
-        
-        textLabel = new JLabel("เบอร์โทร : ");
-        container.add(textLabel);
-        textField = new JTextField(20);
-        container.add(textField);
-        
-        textLabel = new JLabel("ที่อยู่ : ");
-        container.add(textLabel);
-        textArea = new JTextArea(3, 20);
-        container.add(textArea);
+        // JLabel title = new JLabel("เช่าห้อง");
+        // title.setHorizontalAlignment(JLabel.CENTER);
+        // title.setFont(new Font("Tahoma", Font.BOLD, 36));
+        // container.add(title, BorderLayout.NORTH);
 
-        buttonGroup = new ButtonGroup();
-        buttonGroup.add(radioButton = new JRadioButton("ห้องพัดลม (1,800฿)"));
-        container.add(radioButton);
-        buttonGroup.add(radioButton = new JRadioButton("ห้องแอร์ (2,500฿)"));
-        container.add(radioButton);
+        JLabel FnameLabel = new JLabel("ชื่อ : ");
+        container.add(FnameLabel);
+        JTextField FnameField = new JTextField(20);
+        container.add(FnameField);
+        
+        JLabel LnameLabel = new JLabel("นามสกุล : ");
+        container.add(LnameLabel);
+        JTextField LnameField = new JTextField(20);
+        container.add(LnameField);
+        
+        JLabel telLabel = new JLabel("เบอร์โทร : ");
+        container.add(telLabel);
+        JTextField telField = new JTextField(20);
+        container.add(telField);
+        
+        JLabel addressLabel = new JLabel("ที่อยู่ : ");
+        container.add(addressLabel);
+        JTextArea addressField = new JTextArea(3, 20);
+        container.add(addressField);
 
-        checkBox = new JCheckBox("ยอมรับสัญญาการเช่าห้องพัก");
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(fanButton = new JRadioButton("ห้องพัดลม (1,800฿)"));
+        container.add(fanButton);
+        buttonGroup.add(airButton = new JRadioButton("ห้องแอร์ (2,500฿)"));
+        container.add(airButton);
+
+        JCheckBox checkBox = new JCheckBox("ยอมรับสัญญาการเช่าห้องพัก");
         container.add(checkBox);
 
-        confirmButton = new JButton("    ยืนยัน    ");
+        JButton confirmButton = new JButton("    ยืนยัน    ");
         container.add(confirmButton);
 
-        window.setSize( 600, 200);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
+        frame.setSize(600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
