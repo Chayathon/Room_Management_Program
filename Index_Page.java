174ab2cc0946c8930f7b0cc2a2b602b4409e58c2
YANiS <dominyl880@gmail.com>
2024-03-15 19:09:55 +0700

    1

--- Index_Page.java
+++ Index_Page.java
@@ -1,6 +1,36 @@
+import javax.swing.*;
+import java.awt.event.*;
+
 public class Index_Page {
     public static void main(String[] args) {
-        Room_Management RM = new Room_Management();    
+        JFrame frame = new JFrame("Index Page");
+        JButton button = new JButton("Open Room Management");
+        JButton button2 = new JButton("Open Complaint");
+
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // เมื่อปุ่มถูกคลิก
+                Room_Management RM = new Room_Management();
+                // ทำสิ่งที่คุณต้องการเมื่อปุ่มถูกคลิก เช่น เปิดหน้าต่างใหม่
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // เมื่อปุ่มถูกคลิก
+                Complaint Complaint = new Complaint();
+                // ทำสิ่งที่คุณต้องการเมื่อปุ่มถูกคลิก เช่น เปิดหน้าต่างใหม่
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.add(button);
+        panel.add(button2);
+        frame.add(panel);
+        
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
-    
 }
